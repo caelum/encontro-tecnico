2edd36aad3732ec447fbb57170c92a6dba1c84e5
David Paniz <davidpaniz@gmail.com>
2011-08-29 16:22:41 -0300

    ainda tentando arrumar a data no calendar

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -2,18 +2,23 @@ class CalendarController < ApplicationController
   def index
     @calendar = Icalendar::Calendar.new
 
-        Presentation.scheduled.each do |presentation|
-          event = Icalendar::Event.new
-          date = presentation.scheduled_date
-          event.start = DateTime.civil(date.year, date.month, date.day, 13, 0, 0, -3).utc
-          event.end = DateTime.civil(date.year, date.month, date.day, 14, 0, 0, -3).utc
-          event.summary = "#{presentation.user.name} - #{presentation.name}"
-          event.description = presentation.description
-          @calendar.add event
-        end
+    Presentation.scheduled.each do |presentation|
+      event = Icalendar::Event.new
+      date = presentation.scheduled_date
+      event.start = DateTime.civil(date.year, date.month, date.day, 13, 0, 0, -3).utc
+      event.end = DateTime.civil(date.year, date.month, date.day, 14, 0, 0, -3).utc
+      event.summary = "#{presentation.user.name} - #{presentation.name}"
+      event.description = presentation.description
+      @calendar.add event
+    end
 
-        @calendar.publish
-        render :layout => false, :text => @calendar.to_ical
+
+    timezone = Icalendar::Timezone.new
+    timezone.timezone_id = "BRT"
+
+    @calendar.add(timezone)
+    @calendar.publish
+    render :layout => false, :text => @calendar.to_ical
 
   end
 end
